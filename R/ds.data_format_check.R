187ef9a4f20c1569f05f761f0c5a5c98caf55ea8
feddersmichael <fedders.michael@gmail.com>
2023-10-30 16:24:51 +0100

    data_check

---
+++ R/ds.data_format_check.R
@@ -0,0 +1,21 @@
+# We want to check in a generic way if the uploaded data fulfills
+# our requirements to be used in this analysis.
+
+ds.data_format_check <- function(name, datasources = NULL){
+  
+  #TODO needs clarification how exactly the right datasource is checked.
+  if (is.null(datasources)){
+    datasources <- DSI::datashield.connections_find()
+  }
+  
+  # We check if the name is of type character
+  if (!is.character(name)){
+    stop("'name' needs to be a character with the name of the data file.")
+  }
+  
+  # Now we want to see on all servers an object with name 'name' exists.
+  dsBaseClient::ds.testObjExists(name, datasources)
+  
+  # In the next step we check if the file with this name has the right format
+  
+}
