7c4ccbd945b434b78404657749d1e758fb086e4d
feddersmichael <fedders.michael@gmail.com>
2023-11-02 01:36:55 +0100

    add ons

--- R/ds.data_format_check.R
+++ R/ds.data_format_check.R
@@ -1,7 +1,7 @@
 # We want to check in a generic way if the uploaded data fulfills
 # our requirements to be used in this analysis.
 
-ds.data_format_check <- function(name, datasources = NULL){
+ds.data_format_check <- function(name, datasources = NULL, only_numeric = FALSE){
   
   #TODO needs clarification how exactly the right datasource is checked.
   if (is.null(datasources)){
@@ -14,9 +14,21 @@ ds.data_format_check <- function(name, datasources = NULL){
   }
   
   # Now we want to see on all servers an object with name 'name' exists.
-  dsBaseClient::ds.testObjExists(name, datasources)
+  if (!dsBaseClient::ds.exists(name, datasources)){
+    stop(paste0("There doesn't exist a data file with the name ", name))
+  }
   
   # In the next step we check if the file with this name has the right format
-
+  if (!dsBaseClient::ds.class(name, datasources) == "matrix"){
+    stop(paste0("The data needs to be saved as a matrix object instead of ", 
+                dsBaseClient::ds.class(name, datasources)))
+  }
+  
+  # If we activate the numeric option we need to check if all entries of
+  # the matrix are numeric.
+  if (only_numeric){
+    
+  }
+  
 }
 
