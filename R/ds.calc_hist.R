90a65fa04e63d8b8f486c222ea7e7c6c54dd604c
feddersmichael <fedders.michael@gmail.com>
2023-11-14 17:58:18 +0100

    comment

--- R/ds.calc_hist.R
+++ R/ds.calc_hist.R
@@ -1,4 +1,10 @@
 
 ds.calc_hist <- function(){
   
+  # To calculate the histogram which is the 2nd order derivatives of 
+  # the loss function we need the prediction of y under the model up
+  # to this point
+  
+  
+  
 }
