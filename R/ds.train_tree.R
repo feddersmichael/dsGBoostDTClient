e468c79e23d5346febfaf370464b93b1c9773160
feddersmichael <fedders.michael@gmail.com>
2024-01-08 10:20:15 +0100

    almost

--- R/ds.train_tree.R
+++ R/ds.train_tree.R
@@ -32,7 +32,8 @@ ds.train_tree <- function(data_name, last_tr_tree, max_splits = 10,
                              w_s_left = logical(), w_s_left_value = numeric(),
                              w_s_right = logical(), w_s_right_value = numeric(),
                              par_spp = numeric(), par_dir = logical())
-  histograms <- list()
+  
+  split_scores <- list()
   
   # In this loop we build a tree with up to 'max_splits' many splits.
   for (i in 1:max_splits){
@@ -41,91 +42,21 @@ ds.train_tree <- function(data_name, last_tr_tree, max_splits = 10,
                                          spp_cand, current_tree, data_type,
                                          datasources)
     
-    # We search for the next split point.
-    split <- ds.select_split(hist_bins_per_leave, spp_cand)
+    # We search for the best possible split(s) in the newly added branch.
+    best_split <- ds.select_split(hist_bins_per_leave, spp_cand)
     
-    # If the function 'ds.select_split' returns a break criteria instead of a new
-    # split we stop the loop and return the finished tree.
-    if (is.character(split)){
-      break
+    if (nrow(current_tree) == 0){
+      first_split <- c(best_split$feature[1], best_split$split_pt[1],
+                       TRUE, best_split$weight_l[1], TRUE,
+                       best_split$weight_r[1])
+      current_tree <- rbind(current_tree, first_split)
     }
     else {
-      ds.add_split()
+      split_scores <- append(split_scores, best_split)
+      
     }
     
   }
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  # TODO: do we need to update spp every round or just at the start? if they are
-  # permanent we can save them directly on the server
-  # maybe sth like spliting spp_cand methods in group global and local and
-  # change algorithm based on that
-  spp_cand <- ds.gen_spp_cand(data_name, cand_select_mode, datasources)
-
-  tree <- matrix(nrow = 0, ncol = 4)
-  
-  while (TRUE) {
-    # Probably better to find a way to update split points in the one feature
-    # where we added a split -> the rest should stay the same
-    spp_cand <- ds.gen_spp(data_name, cand_select_mode, datasources)
-    
-    # ---
-    # from original big exact splitting point function
-    # ---
-    # from server need y_i and \hat{y}_i and maybe data security level
-    
-    # check for data sensibility at server
-    # 1: data can be shared
-    # 2: histograms can be shared
-    # 3: only optimal splitting point on local data is shared
-    
-    # Tasks which need to be performed:
-    # go through all possible splits
-    # calculate split score fore each split
-    #   this include summing up histograms -> Secure Multi-Party Computation (MPC)
-    # save all or only highest... ?
-    # select highest split to expand tree
-    
-    ds.add_split()
-    
-    hist <- ds.calc_hist()
-    
-    # Probably just different feedback if ssc <= 0 instead of sending back all
-    # Shall send back always the best combination. If that isn't good enough
-    # we can just end tree building process.
-    split_list <- ds.calc_spsc()
-    
-    if (ssc > 0){
-      
-      # probably faster to only change affected rows instead of copying 
-      # everything
-      tree <- ds.add_split(split_list[2:4], tree)
-      # TODO How and if data is affected by splitting for effective programming
-      
-    }
-    # More criteria like fixed max amount of iterations or sth possible
-    else {
-      break
-    }
-    
-  }
-  
-  # Now we can give back the tree
-  
-  return(tree)
+  return(current_tree)
 }
