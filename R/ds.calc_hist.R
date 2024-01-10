91caeed8f6ee77f517f1b81ddb42ab9cd9672a98
feddersmichael <fedders.michael@gmail.com>
2024-01-10 12:11:38 +0100

    debug 1

--- R/ds.calc_hist.R
+++ R/ds.calc_hist.R
@@ -10,29 +10,29 @@
 #' @return None.
 #' @export
 ds.calc_hist <- function(data_name, last_tr_tree, loss_function,
-                         datasources = NULL){
-  
+                         datasources = NULL) {
+
   # We first check all the inputs for appropriate class and set defaults if
   # no input is given.
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
-  } 
-  else if (!DSI:::.isDSConnection(datasources)) {
+  }
+  else if (!all(sapply(datasources, DSI:::.isDSConnection))) {
     stop("'datasources' needs to be a an object of the 'DSConnection' class.")
   }
-  
-  if (!is.character(data_name)){
+
+  if (!is.character(data_name)) {
     stop("'data_name' needs to have data type 'character'.")
   }
-  
-  if (!is.character(loss_function)){
+
+  if (!is.character(loss_function)) {
     stop("'loss_function' needs to have data type 'character'.")
   }
   # We call the server to generate the new histogram values based on the
   # predicted output, updated through the last trained tree
-  
+
   # If 'last_tr_tree' is NULL we initialise the predicted output with 0's.
-  if (is.null(last_tr_tree)){
+  if (is.null(last_tr_tree)) {
     cally <- call("calc_hist_initDS", data_name, loss_function)
     output <- DSI::datashield.assign.expr(datasources, 
                                           paste0(data_name, "_training"), cally)
@@ -40,12 +40,12 @@ ds.calc_hist <- function(data_name, last_tr_tree, loss_function,
   # if we already trained a tree before we just add up the predicted value from
   # the last trained tree
   else {
-    if (!is.data.frame(last_tr_tree)){
+    if (!is.data.frame(last_tr_tree)) {
       stop("'last_tr_tree' needs to be an object of type 'data frame'.")
     }
-    
+
     cally <- call("calc_histDS", data_name, last_tr_tree, loss_function)
-    output <- DSI::datashield.assign.expr(datasources, 
+    output <- DSI::datashield.assign.expr(datasources,
                                           paste0(data_name, "_training"), cally)
   }
   
--- R/ds.create_data_split.R
+++ R/ds.create_data_split.R
@@ -10,80 +10,71 @@
 #'
 #' @return None.
 #' @export
-ds.create_data_split <- function(data_name, train_test_ratio, 
-                                 split_status = NULL, datasources = NULL){
-  
+ds.create_data_split <- function(data_name, train_test_ratio = 0.8,
+                                 split_status = NULL, datasources = NULL) {
+
   # We first check all the inputs for appropriate class and set defaults if
   # no input is given.
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
-  } 
-  else if (!DSI:::.isDSConnection(datasources)) {
+  }
+  else if (!all(sapply(datasources, DSI:::.isDSConnection))) {
     stop("'datasources' needs to be a an object of the 'DSConnection' class.")
   }
-  
+
   if (is.null(split_status)){
-    if (!is.character(data_name) || !(length(data_name) != 1)){
+    if (!is.character(data_name) || !(length(data_name) != 2)){
       stop("'data_name' needs to be an atomic vector with data type 'character'.")
     }
-    
-    if (!is.numeric(train_test_ratio) || (numeric(train_test_ratio) < 0) || 
-        (numeric(train_test_ratio) > 1)){
-      stop(paste0("'train_test_ratio' needs to have data type 'numeric' and lie",
-                  " between 0 and 1."))
+
+    if (!is.numeric(train_test_ratio) || (numeric(train_test_ratio) < 0) ||
+          (numeric(train_test_ratio) > 1)) {
+      stop("'train_test_ratio' needs to have data type 'numeric' and lie between 0 and 1.")
     }
-    
+
     # We split up the dataset in a training and test part.
-    cally <- call("create_data_splitDS", data_name, train_test_ratio,
+    cally <- call("create_data_splitDS", data_name[1], train_test_ratio,
                   split_status)
-    result <- DSI::datashield.assign.expr(datasources, 
-                                          paste0(data_name,
-                                                 "_training_test_split"), 
+    result <- DSI::datashield.assign.expr(datasources,
+                                          paste0(data_name[2],
+                                                 "_training_test_split"),
                                           cally)
-    
-    cally <- call("save_trainingDS", data_name)
-    result <- DSI::datashield.assign.expr(datasources, paste0(data_name,
-                                                              "_training"), 
-                                          cally)
+
+    data_name <- data_name[2]
   }
-  else if (split_status == 'Train'){
-    if (!is.character(data_name) || !(length(data_name) != 2)){
+  else if (split_status == 'Train') {
+    if (!is.character(data_name) || length(data_name) != 2) {
       stop("'data_name' needs to be an object of type 'vector' with data type 'character' and length 2.")
     }
-    
+
     # We save the training data-set under a general name.
     cally <- call("create_data_splitDS", data_name[1], train_test_ratio,
                   split_status)
     result <- DSI::datashield.assign.expr(datasources, 
                                           paste0(data_name[2],
-                                                 "_training_test_split"), 
+                                                 "_training_test_split"),
                                           cally)
-    
-    cally <- call("save_trainingDS", data_name)
-    result <- DSI::datashield.assign.expr(datasources, paste0(data_name[2],
-                                                              "_training"), 
-                                          cally)
+
+    data_name <- data_name[2]
   }
-  else if (split_status == 'Train_Test'){
-    if (!is.character(data_name) || !(length(data_name) != 3)){
+  else if (split_status == 'Train_Test') {
+    if (!is.character(data_name) || length(data_name) != 3) {
       stop("'data_name' needs to be an object of type 'vector' with data type 'character' and length 3.")
     }
-    
+
     # We save the training data-set under a general name.
     cally <- call("create_data_splitDS", data_name[1:2],
                   train_test_ratio, split_status)
-    result <- DSI::datashield.assign.expr(datasources, 
+    result <- DSI::datashield.assign.expr(datasources,
                                           paste0(data_name[3],
-                                                 "_training_test_split"), 
+                                                 "_training_test_split"),
                                           cally)
-    
-    cally <- call("save_trainingDS", data_name)
-    result <- DSI::datashield.assign.expr(datasources, paste0(data_name[3],
-                                                              "_training"), 
-                                          cally)
+
+    data_name <- data_name[3]
   }
   else {
     stop("'split_status' needs to be either 'NULL' or an atomic vector with data type 'character'.")
   }
-  
+
+  return(data_name)
 }
